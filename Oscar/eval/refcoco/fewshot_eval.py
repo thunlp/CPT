d132cafe2366c797421441752d5528c1abd3b91e
waxnkw <zhanga6@outlook.com>
2022-05-11 19:57:59 +0800

    add zsl eval

--- Oscar/eval/refcoco/fewshot_eval.py
+++ Oscar/eval/refcoco/fewshot_eval.py
@@ -25,7 +25,7 @@ def get_str(l):
         accs.append(x)
     return "acc is {:.2f}, std is {:.2f}".format(np.mean(accs), np.std(accs))
 
-for i in [0, 16, 8, 4, 2, 1]:
+for i in [16, 8, 4, 2, 1]:
     i = str(i)
     color_root = os.path.join(sys.argv[1], str(i))
     if not os.path.exists(color_root):
@@ -44,5 +44,21 @@ for i in [0, 16, 8, 4, 2, 1]:
     print("---------------------------------------------")
     
 
+# zero shot
+import json
+def zsl_str(p):
+    lines = list(open(p, "r").readlines())
+    line = lines[-1]
+    s = "["+line.split("[")[-1]
+    s = json.loads(s)
+    return s
 
+color_root = os.path.join(sys.argv[1], "0")
+splits = ["refcoco", "refcoco+", "refcocog"]
+for sp in splits:
+    p = glob.glob(os.path.join(color_root, sp, "*"))[0]
+    rst_list = zsl_str(p)
+    for j, x in enumerate(["_val", "_testA", "_testB"]):
+        if j < len(rst_list):
+            print(sp+x+":", "\t", "acc is {:.2f}".format(rst_list[j]))
 
