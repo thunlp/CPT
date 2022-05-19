8e7173cf516070d1e9e46b2324632d369409da17
waxnkw <zhanga6@outlook.com>
2022-05-19 14:51:10 +0800

    use more steps for feat extract

--- prompt_feat/cmds/vg/vg_test.sh
+++ prompt_feat/cmds/vg/vg_test.sh
@@ -2,7 +2,10 @@
 
 rm -rf output/vg/test/inference/
 
-for i in 0 1 2 3 4
+for((i=0;i<=14;i++));
 do
-        sh cmds/vg/_vg_test.sh 5 $i
+    bash cmds/vg/_vg_test.sh 15 $i
 done
+#for i in 0 1 2 3 4 5 6 7 8 9
+#do
+#done
