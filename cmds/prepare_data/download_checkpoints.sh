c0347e8ce2d4bb09ce264586b82adbee273333de
waxnkw <zhanga6@outlook.com>
2022-11-01 10:15:53 +0800

    use my cached model links

--- cmds/prepare_data/download_checkpoints.sh
+++ cmds/prepare_data/download_checkpoints.sh
@@ -1,5 +1,6 @@
 cd Oscar/
-wget https://biglmdiag.blob.core.windows.net/vinvl/model_ckpts/image_captioning/pretrained_base.zip
+#wget https://biglmdiag.blob.core.windows.net/vinvl/model_ckpts/image_captioning/pretrained_base.zip
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/pretrained_base.zip
 unzip pretrained_base.zip
 mkdir -p pretrained_models/image_captioning/pretrained_base/
 mv pretrained_base/checkpoint-2000000/* pretrained_models/image_captioning/pretrained_base/
@@ -10,7 +11,8 @@ cd ..
 cd prompt_feat/
 mkdir -p models/vinvl
 cd models/vinvl
-wget https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/vinvl_vg_x152c4.pth
+#wget https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/vinvl_vg_x152c4.pth
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/vinvl_vg_x152c4.pth
 cd ../../..
 
 
